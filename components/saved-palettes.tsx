d670be4624b8e90833056c9501e00e1ff3fa5630
ido evergeen <dicksonevergreenido@gmail.com>
2024-04-01 04:23:24 +0100

    add overlay

--- components/saved-palettes.tsx
+++ components/saved-palettes.tsx
@@ -1,4 +1,5 @@
 import { client } from "@/config/client";
+import { useMediaQuery } from "@/hooks/use-media-query";
 import { Database } from "@/types/supabase";
 import Link from "next/link";
 import React, { useEffect, useState } from "react";
@@ -26,41 +27,91 @@ export default function SavedPalettes() {
     fetchUserPalettes();
   }, []);
 
-  return (
-    <div className="pt-32 px-3 lg:w-full w-[90%] top-0  h-screen right-0 fixed lg:sticky bg-white ">
-      <div className="relative bottom-0 left-0  rounded-lg     px-3 pb-4 pt-1   w-full">
-        {isLoading ? (
-          <div className="flex justify-center items-center">
-            <div className="border-gray-300 h-10 w-10 animate-spin rounded-full border-2 border-t-black" />
-          </div>
-        ) : null}
+  const isDesktop = useMediaQuery("(min-width: 768px)");
 
-        {!isLoading &&
-          SavedPalettes?.map((palette) => {
-            return (
-              <>
-                <Link
-                  href={`/color/${palette.colors}`}
-                  className="flex  w-full m-3"
-                >
-                  {palette.colors?.split("-").map((i) => {
-                    return (
-                      <div
-                        className="flex-1 flex justify-center items-center cursor-pointer  first:rounded-l-lg last:rounded-r-lg   h-[37px]"
-                        key={palette.id}
-                        style={{
-                          background: `#${i}`,
-                        }}
-                      ></div>
-                    );
-                  })}
-                </Link>
+  if (!isDesktop) {
+    return (
 
-                <p className="text-xs m-3">{palette.title}</p>
-              </>
-            );
-          })}
+        <>
+      <div className="absolute left-0 z-0 top-14 h-screen w-full bg-black opacity-40"></div>
+
+
+<div className="pt-32 px-3 lg:w-full w-[90%] top-0  h-screen right-0 fixed lg:sticky bg-white ">
+        <div className="relative bottom-0 left-0  rounded-lg     px-3 pb-4 pt-1   w-full">
+          {isLoading ? (
+            <div className="flex justify-center items-center">
+              <div className="border-gray-300 h-10 w-10 animate-spin rounded-full border-2 border-t-black" />
+            </div>
+          ) : null}
+
+          {!isLoading &&
+            SavedPalettes?.map((palette) => {
+              return (
+                <>
+                  <Link
+                    href={`/color/${palette.colors}`}
+                    className="flex  w-full m-3"
+                  >
+                    {palette.colors?.split("-").map((i) => {
+                      return (
+                        <div
+                          className="flex-1 flex justify-center items-center cursor-pointer  first:rounded-l-lg last:rounded-r-lg   h-[37px]"
+                          key={palette.id}
+                          style={{
+                            background: `#${i}`,
+                          }}
+                        ></div>
+                      );
+                    })}
+                  </Link>
+
+                  <p className="text-xs m-3">{palette.title}</p>
+                </>
+              );
+            })}
+        </div>
       </div>
-    </div>
+        </>
+    );
+  }
+
+  return (
+    <>
+      <div className="pt-32 px-3 lg:w-full w-[90%] top-0  h-screen right-0 fixed lg:sticky bg-white ">
+        <div className="relative bottom-0 left-0  rounded-lg     px-3 pb-4 pt-1   w-full">
+          {isLoading ? (
+            <div className="flex justify-center items-center">
+              <div className="border-gray-300 h-10 w-10 animate-spin rounded-full border-2 border-t-black" />
+            </div>
+          ) : null}
+
+          {!isLoading &&
+            SavedPalettes?.map((palette) => {
+              return (
+                <>
+                  <Link
+                    href={`/color/${palette.colors}`}
+                    className="flex  w-full m-3"
+                  >
+                    {palette.colors?.split("-").map((i) => {
+                      return (
+                        <div
+                          className="flex-1 flex justify-center items-center cursor-pointer  first:rounded-l-lg last:rounded-r-lg   h-[37px]"
+                          key={palette.id}
+                          style={{
+                            background: `#${i}`,
+                          }}
+                        ></div>
+                      );
+                    })}
+                  </Link>
+
+                  <p className="text-xs m-3">{palette.title}</p>
+                </>
+              );
+            })}
+        </div>
+      </div>
+    </>
   );
 }
